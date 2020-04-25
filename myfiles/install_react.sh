f92c84c08170b5051f45c8ce1755eac4c13be385
Yasuhiro Osajima <kimukimu33@gmail.com>
2020-04-26 04:11:13 +0900

    update react installation

--- myfiles/install_react.sh
+++ myfiles/install_react.sh
@@ -7,28 +7,8 @@
 # npm install -g yarn
 
 # yarn create react-app my-app --template typescript
-
 # yarn add -D prettier prettier-eslint prettier-eslint-cli
 # yarn add -D eslint-config-prettier eslint-plugin-prettier
-# yarn add -D eslint-config-airbnb
-
-# ./node_modules/.bin/eslint --init
-# yarn add -D eslint-plugin-react@^7.19.0 @typescript-eslint/eslint-plugin@latest eslint-config-airbnb@latest eslint@^5.16.0 || ^6.8.0 eslint-plugin-import@^2.20.1 eslint-plugin-jsx-a11y@^6.2.3 eslint-plugin-react-hooks@^2.5.0 || ^1.7.0 @typescript-eslint/parser@latest
-# yarn add -D eslint@^6.6.0
-
-# vim package.json
-# 
-# "eslintConfig": {
-#     "extends": [
-#       "react-app",
-#       "airbnb"     <-
-#     ],
-#     "settings": {
-#       "react": {
-#         "version": "16.11"
-#       }
-#     }
-#   },
 
 
 pkglist=(
