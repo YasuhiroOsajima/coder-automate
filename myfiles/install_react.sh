3afe43ed0c4ff51b7bc92388b28dfc615bba2e5f
Yasuhiro Osajima <kimukimu33@gmail.com>
2020-04-26 02:43:19 +0900

    update react installation

--- myfiles/install_react.sh
+++ myfiles/install_react.sh
@@ -14,6 +14,7 @@
 
 # ./node_modules/.bin/eslint --init
 # yarn add -D eslint-plugin-react@^7.19.0 @typescript-eslint/eslint-plugin@latest eslint-config-airbnb@latest eslint@^5.16.0 || ^6.8.0 eslint-plugin-import@^2.20.1 eslint-plugin-jsx-a11y@^6.2.3 eslint-plugin-react-hooks@^2.5.0 || ^1.7.0 @typescript-eslint/parser@latest
+# yarn add -D eslint@^6.6.0
 
 # vim package.json
 # 
