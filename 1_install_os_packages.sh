ea714422f68d2e741624054cc0f965d088ad53f9
Yasuhiro Osajima <kimukimu33@gmail.com>
2020-09-01 23:36:57 +0900

    update to use docker with centos7

--- 1_install_os_packages.sh
+++ 1_install_os_packages.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
+echo "alias ls='ls --color=auto'" >> ~/.bashrc
+
 yum update -y
-yum install podman tmux git -y
+yum install tmux git -y
 cp tmux.conf ~/.tmux.conf
+
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce docker-ce-cli containerd.io
+systemctl enable docker
+
+curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
 reboot
