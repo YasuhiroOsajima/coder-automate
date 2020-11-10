602902a57704d995744282cfab69cecd05ead427
Yasuhiro Osajima <kimukimu33@gmail.com>
2020-11-10 21:44:13 +0900

    update docker installation

--- myfiles/install_docker.sh
+++ myfiles/install_docker.sh
@@ -3,10 +3,14 @@
 curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
+apt update
 apt upgrade -y
+
 apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs)  stable"
+
 apt update
 apt install docker-ce docker-ce-cli containerd.io -y
 
